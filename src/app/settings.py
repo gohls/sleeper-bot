4f76e8737ebbec329ae8189e6d698cc32cbfc76b
Simon Gohl <sagohl@gmail.com>
2024-09-05 18:54:30 -0700

    set staticfiles

--- src/app/settings.py
+++ src/app/settings.py
@@ -74,8 +74,7 @@ TEMPLATES = [
     },
 ]
 
-print(BASE_DIR)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'website/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'website/static'),
