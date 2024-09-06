07a3855f079a446903206c564b031eb102a074cf
Simon Gohl <sagohl@gmail.com>
2024-09-05 20:09:14 -0700

    update security

--- src/app/settings.py
+++ src/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ PASSWORD = os.getenv('PASSWORD')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9a)6y2e(b4j@-+!3q3t$(5t9zaq@)32p2oqm$wc13mej=y+#!-"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
